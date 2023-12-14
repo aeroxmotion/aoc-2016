dd617bce5369828e404bbfe52cbeb4560f99a8c3
Camilo Rodriguez <aeroxmotion@hotmail.com>
2023-12-13 19:27:51 -0500

    day10

---
+++ lib/day10.test.ts
@@ -0,0 +1,94 @@
+import { test, expect } from "bun:test";
+
+import { readInput } from "./utils";
+
+let input = await readInput(10);
+
+type Type = "bot" | "output";
+
+class RecordQueue<T = number> {
+  data: Record<number, T[]> = {};
+
+  put(n: number, d: T, sort = true) {
+    const data = this.data[n] || (this.data[n] = []);
+
+    data.push(d);
+
+    if (sort && data.length >= 2) {
+      (data as number[]).sort((a, b) => a - b);
+    }
+
+    return data.length;
+  }
+
+  get(n: number) {
+    const result = this.data[n];
+    this.data[n] = [];
+
+    return result;
+  }
+}
+
+function solution(output = false) {
+  let startBot = -1;
+  const transitions = new RecordQueue<[Type, number]>();
+  const types: Record<Type, RecordQueue> = {
+    bot: new RecordQueue(),
+    output: new RecordQueue(),
+  };
+
+  for (const line of input.split("\n")) {
+    const parts = line.split(" ");
+
+    // Initial chip -> bot
+    if (line.startsWith("value")) {
+      const bot = +parts[5];
+
+      if (types.bot.put(bot, +parts[1]) >= 2) {
+        startBot = bot;
+      }
+
+      // Transition bot -> <low:<target>|high:<target>>
+    } else {
+      const from = +parts[1];
+
+      // Put low
+      transitions.put(from, [parts[5] as Type, +parts[6]], false);
+
+      // Put high
+      transitions.put(from, [parts[10] as Type, +parts[11]], false);
+    }
+  }
+
+  const stack = [startBot];
+
+  while (stack.length) {
+    const bot = stack.pop()!;
+    const [lowChip, highChip] = types.bot.get(bot);
+    const [[lowType, lowTarget], [highType, highTarget]] = transitions.get(bot);
+
+    if (lowChip === 17 && highChip === 61 && !output) {
+      return bot;
+    }
+
+    if (types[lowType].put(lowTarget, lowChip) >= 2 && lowType === "bot") {
+      stack.push(lowTarget);
+    }
+
+    if (types[highType].put(highTarget, highChip) >= 2 && highType === "bot") {
+      stack.push(highTarget);
+    }
+  }
+
+  return (
+    types.output.get(0)[0] * types.output.get(1)[0] * types.output.get(2)[0]
+  );
+}
+
+test("part1", () => {
+  expect(solution()).toBe(47);
+});
+
+test("part2", () => {
+  expect(solution(true)).toBe(2_666);
+});
